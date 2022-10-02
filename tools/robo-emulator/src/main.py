134928771c92dc944e30aafa408d0d0cebb4593a
Raisess <danilosantana456789@gmail.com>
2022-10-02 20:50:58 -0300

    tools::robo-emulator: fix tab size

--- tools/robo-emulator/src/main.py
+++ tools/robo-emulator/src/main.py
@@ -14,13 +14,13 @@ BUILD_DIR = "./build"
 BUILD_OUT = BUILD_DIR + "/main.o"
 
 if __name__ == "__main__":
-    ino_file = sys.argv[1]
+  ino_file = sys.argv[1]
 
-    os.system(f"cp {ino_file} ./cpp-lib/main.ino.cpp")
-    os.system("rm -rf " + BUILD_DIR)
-    os.system("mkdir -p " + BUILD_DIR)
-    os.system(f"{CXX} {LIB_INCLUDE} {LIB_SRC} ./cpp-lib/main.ino.cpp ./cpp-lib/main.cpp -o {BUILD_OUT}")
+  os.system(f"cp {ino_file} ./cpp-lib/main.ino.cpp")
+  os.system("rm -rf " + BUILD_DIR)
+  os.system("mkdir -p " + BUILD_DIR)
+  os.system(f"{CXX} {LIB_INCLUDE} {LIB_SRC} ./cpp-lib/main.ino.cpp ./cpp-lib/main.cpp -o {BUILD_OUT}")
 
-    print("\t\tRunning with ROBO Emulator\n")
+  print("\t\tRunning with ROBO Emulator\n")
 
-    os.system(BUILD_OUT)
+  os.system(BUILD_OUT)
